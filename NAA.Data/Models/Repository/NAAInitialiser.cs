d768086a717e0824ffd00ac8a4d3b7449ca83cd7
Jaguczak, Nicholas <nicholas.jaguczak@student.fhws.de>
2021-06-10 14:52:03 +0200

    Create context,initialiser, DAO, Service

---
+++ NAA.Data/Models/Repository/NAAInitialiser.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NAA.Data.Models.Domain;
+
+namespace NAA.Data.Models.Repository
+{
+    public class NAAInitialiser : System.Data.Entity.DropCreateDatabaseIfModelChanges<NAAContext>
+    {
+        protected override void Seed(NAAContext context)
+        {
+            University university = new University
+            {
+                Name = "Sheffield"
+            };
+            University university2 = new University
+            {
+                Name = "Sheffield Hallam"
+            };
+
+            context.Universities.Add(university);
+            context.Universities.Add(university2);
+
+
+            context.SaveChanges();
+        }
+    }
+}
