84f284d3bdbed5012c14f50bc5f961302096f298
Jaguczak, Nicholas <nicholas.jaguczak@student.fhws.de>
2021-06-10 15:35:05 +0200

    Create Database

--- NAA/App_Start/RouteConfig.cs
+++ NAA/App_Start/RouteConfig.cs
@@ -14,6 +14,12 @@ namespace NAA
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
+                name: "Universities",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "University", action = "Index", id = UrlParameter.Optional }
+            );
+
+            routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
